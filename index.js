1dcf6056d65bbd59b0e556f4376a847df47b5d42
Dave Nicolson <david.nicolson@gmail.com>
2022-10-07 23:26:38 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const { mkdtempSync, removeSync } = require('fs-extra');
+const sqlite3 = require('sqlite3').verbose();
+const convertCpbitmapToPng = require('cpbitmap-to-png');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const IRestore = require('irestore');
+
+const extractWallpapers = (backupPath, useOriginalFilename) => {
+    const db = new sqlite3.Database(`${backupPath}/Manifest.db`);
+    return new Promise((resolve, reject) => {
+        const files = [];
+        db.each(`SELECT * FROM Files WHERE "relativePath" LIKE "%Background.cpbitmap%"`, (_err, row) => {
+            if (row) {
+                files.push({
+                    originalFilename: row.relativePath.replace(/^Library\/SpringBoard\//, ''),
+                    path: useOriginalFilename ? row.relativePath : `${row.fileID.slice(0,2)}/${row.fileID}`,
+                });
+                resolve(files);
+            } else {
+                reject('Encrypted backup?');
+            }
+        });
+    });
+}
+
+const decryptBackup = async (backupPath) => {
+    const tempBackupPath = mkdtempSync('irestore');
+    const iRestore = new IRestore(backupPath);
+    await iRestore.restore('HomeDomain', tempBackupPath);
+    return tempBackupPath;
+}
+
+const main = async () => {
+    const argv = yargs(hideBin(process.argv)).argv;
+    const backupPath = argv._[0];
+    const outputPath = argv.o || '';
+    let tempBackupPath;
+    let files;
+
+    try {
+        files = await extractWallpapers(backupPath);
+    } catch (e) {
+        console.log('Manifest.db appears to be encrypted.\n\nEnter backup password:');
+
+        tempBackupPath = await decryptBackup(backupPath);
+
+        try {
+            files = await extractWallpapers(tempBackupPath, true);
+        } catch (e) {
+            console.log('Manifest.db file is unreadable.');
+        }
+    }
+
+    files && files.forEach(file => {
+        const oldFile = path.join(tempBackupPath || backupPath, file.path);
+        const newFile = path.join(outputPath, file.originalFilename.replace(/cpbitmap$/, 'png'));
+        convertCpbitmapToPng(oldFile, newFile);
+    });
+
+    if (tempBackupPath) {
+        removeSync(tempBackupPath);
+    }
+}
+
+main();
