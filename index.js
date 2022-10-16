258df530f7fb907aa9d0a34e3f5db252f6189018
Dave Nicolson <david.nicolson@gmail.com>
2022-10-16 21:27:48 +0200

    0.1

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+const os = require('os');
 const path = require('path');
 const fs = require('fs-extra');
 const sqlite3 = require('sqlite3').verbose();
 const convertCpbitmapToPng = require('cpbitmap-to-png');
 const IRestore = require('irestore');
-const plist = require('plist');
+const plist = require("simple-plist");
 
-const extractWallpapers = (backupPath, useOriginalFilename) => {
+const extractWallpapers = (backupPath, useOriginalFilename = false) => {
     const db = new sqlite3.Database(`${backupPath}/Manifest.db`);
 
     return new Promise((resolve, reject) => {
         const files = [];
-        db.each(`SELECT * FROM Files WHERE "relativePath" LIKE "%Background.cpbitmap%"`, (_err, row) => {
+        db.each('SELECT * FROM Files WHERE "relativePath" LIKE "%Background.cpbitmap%"', (_err, row) => {
             if (row) {
                 files.push({
                     originalFilename: row.relativePath.replace(/^Library\/SpringBoard\//, ''),
@@ -25,25 +26,25 @@ const extractWallpapers = (backupPath, useOriginalFilename) => {
 }
 
 const getiOSVersion = (backupPath) => {
-    const infoPlist = plist.parse(fs.readFileSync(`${backupPath}/Info.plist`, 'utf8'));
+    const infoPlist = plist.readFileSync(`${backupPath}/Info.plist`, 'utf8');
     return parseInt(infoPlist['Product Version'], 10);
 }
 
 const decryptBackup = async (backupPath) => {
-    const tempBackupPath = fs.mkdtempSync('irestore');
+    const tempBackupPath = fs.mkdtempSync(path.join(os.tmpdir(), 'irestore'));
     const iRestore = new IRestore(backupPath);
     await iRestore.restore('HomeDomain', tempBackupPath);
     return tempBackupPath;
 }
 
 const main = async () => {
-    if (process.argv.length < 4) {
-        console.log('Need at least two args: input filename and result filename [--ios-version iOS version]')
-        console.log(`Example: ${process.argv[0]} ${process.argv[1]} HomeBackground.cpbitmap HomeBackground.png [--ios-version 12]`)
-        return
+    if (process.argv.length != 4) {
+      console.log('Usage: ios-wallpaper-extractor <backup path> <output directory>');
+      process.exit(1);
     }
-    const backupPath = process.argv[2]
-    const outputPath = process.argv[3]
+
+    const backupPath = process.argv[2];
+    const outputPath = process.argv[3];
 
     let tempBackupPath;
     let files = [];
